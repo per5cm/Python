ac8ec94d3017c1eee3e066024636dfe448f029dc
per5cm <per5cm@proton.me>
2024-08-19 19:34:38 +0200

    smol bump

--- Beginner/CS50/Lecture/Lecture2/cat.py
+++ Beginner/CS50/Lecture/Lecture2/cat.py
@@ -33,24 +33,24 @@
 
 #sixsth version
 
-#def main():
-#    number = get_number()
-#    meow(number)
-#
-#def get_number():
-#    while True:
-#        n = int(input("What's n? "))
-#        if n > 0:
-#            break
-#        return n
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            break
+        return n
 
 
-#def meow(n):
-#    for _ in range(n):
-#        print("meow")
-#
-#
-#main()
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+
+main()
 
 
 
