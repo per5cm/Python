ff9b5cb4eb1199160b939859533f1165b10cf2d4
per5cm <per5cm@proton.me>
2024-08-23 14:57:52 +0200

    bump

--- Beginner/CS50/Problems/Lecture2/plates.py
+++ Beginner/CS50/Problems/Lecture2/plates.py
@@ -6,8 +6,26 @@ def main():
         print("Invalid")
 
 
-def is_valid(s):
-    ...
+def is_valid(plate):
+    # Check lenght
+    if len(plate) < 2 or len(plate) > 6:
+        return False
+    # First characters must be letters
+    if not (plate[:2].isalpha()):
+        return False
+    # All characters must be alphanumeric
+    if not plate.isalnum():
+        return False
+    # Check the transition between letters and numbers
+    for item in range(len(plate)-1):
+        if plate[item].isdigit() and plate[item + 1].isalpha():
+            return False
+    # If there are digits, they must not start with '0'
+    for item in plate:
+        if item.isdigit():
+            return item != '0'
+    else:
+        return True
 
 
 main()
