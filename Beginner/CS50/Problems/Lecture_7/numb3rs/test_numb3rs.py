c48119c5accd3c962c1a607ccc202461fb381643
per5cm <per5cm@proton.me>
2024-09-23 11:26:34 +0200

    test_numb3rs.py

---
+++ Beginner/CS50/Problems/Lecture_7/numb3rs/test_numb3rs.py
@@ -0,0 +1,13 @@
+import pytest
+from numb3rs import validate
+
+def test_validate_ip():
+    assert validate(r"127.0.0.1") == True
+    assert validate(r"255.255.255.255") == True
+
+def test_validate_range():
+    assert validate(r"cat") == False
+    assert validate(r"512.512.512.512") == False
+    assert validate(r"1.2.3.1000") == False
+
+
