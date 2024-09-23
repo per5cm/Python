9d4f5ff53cb9a2157fd156bbf15e5d998c886d57
per5cm <per5cm@proton.me>
2024-09-23 12:06:32 +0200

    update test_numb3rs.py

--- Beginner/CS50/Problems/Lecture_7/numb3rs/test_numb3rs.py
+++ Beginner/CS50/Problems/Lecture_7/numb3rs/test_numb3rs.py
@@ -9,5 +9,6 @@ def test_validate_range():
     assert validate(r"cat") == False
     assert validate(r"512.512.512.512") == False
     assert validate(r"1.2.3.1000") == False
+    assert validate(r"1.512.1.1") == False
 
 
