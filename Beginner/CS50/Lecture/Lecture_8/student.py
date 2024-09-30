ab10a768c1400a08e5003e6501387626f62944ae
per5cm <per5cm@proton.me>
2024-09-30 10:22:37 +0100

    tuple

--- Beginner/CS50/Lecture/Lecture_8/student.py
+++ Beginner/CS50/Lecture/Lecture_8/student.py
@@ -29,4 +29,21 @@ def get_student():
 
 if __name__ == "__main___":
     main()
-        
+
+
+## Using tuple.        
+
+def main():
+    student = get_student()
+    if student[0] == "Padma":
+        student[1] = "Ravenclaw"
+    print(f"{student[0]} from {student[1]}")
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return [name, house]
+
+
+if __name__ == "__main___":
+    main()
