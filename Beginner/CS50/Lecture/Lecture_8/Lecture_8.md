251d21cfaf9f745e8ececdbb49451740ec7d6507
per5cm <per5cm@proton.me>
2024-09-30 10:15:24 +0100

    student.py

---
+++ Beginner/CS50/Lecture/Lecture_8/Lecture_8.md
@@ -0,0 +1,5 @@
+# Object Oriented Programming
+
+# tuple
+
+A tuple is another type of data in Python tha's a collection of values. its similar in spirit to a list, in that sense, but it's immutable.
---
+++ Beginner/CS50/Lecture/Lecture_8/student.py
@@ -0,0 +1,32 @@
+## Initial version
+
+def main():
+    name = get_name()
+    house = get_house()
+    print(f"{name} from {house}")
+
+def get_name():
+    return input("Name: ")
+
+def get_house():
+    return input("House: ")
+
+
+if __name__ == "__main___":
+    main()
+    
+## Next solution.
+
+def main():
+    name, house = get_student()
+    print(f"{name} from {house}")
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return name, house
+
+
+if __name__ == "__main___":
+    main()
+        
