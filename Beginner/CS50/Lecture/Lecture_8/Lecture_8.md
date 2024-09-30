521941ef854a219a0ee0ba33dd27a17c9813e7b2
per5cm <per5cm@proton.me>
2024-09-30 11:12:31 +0100

    class

--- Beginner/CS50/Lecture/Lecture_8/Lecture_8.md
+++ Beginner/CS50/Lecture/Lecture_8/Lecture_8.md
@@ -11,3 +11,19 @@ A class allows you to define a custom container.
 
 'docs.python.org/3/tutorial/classes.html'
 
+# objects
+
+like a blueprint for a house or a class is like a mold, an object is when you use that blueprint to build a specific house.
+You creat objects from classes.
+
+# atributes or instance variables
+
+wtf is atributes.
+
+# methods
+
+classes come with certain methods, or functions inside them, that you can define, and they just behave in special way.
+
+# __init__
+
+Dunder init method.
--- Beginner/CS50/Lecture/Lecture_8/student.py
+++ Beginner/CS50/Lecture/Lecture_8/student.py
@@ -103,7 +103,6 @@ class Student:
     ...
 
 
-
 def main():
     student = get_student()
     print(f"{student.name} from {student.house}")
@@ -112,6 +111,27 @@ def get_student():
     student = Student()
     student.name = input("Name: ")
     student.house = input("House: ")
+    return student
+
+
+if __name__ == "__main__":
+    main()
+    
+# classes futher implemetations with methods.
+
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    student = Student(name, house)
     return student
 
 
