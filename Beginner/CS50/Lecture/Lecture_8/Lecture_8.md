467906ba2bf3664bc8f946e21c06277dc27cf30c
per5cm <per5cm@proton.me>
2024-09-30 10:51:04 +0100

    class

--- Beginner/CS50/Lecture/Lecture_8/Lecture_8.md
+++ Beginner/CS50/Lecture/Lecture_8/Lecture_8.md
@@ -3,3 +3,11 @@
 # tuple
 
 A tuple is another type of data in Python tha's a collection of values. its similar in spirit to a list, in that sense, but it's immutable.
+
+# classes
+
+A class is like a blueprint for pieces of data objects. A class is a mold that you can define and give a name.
+A class allows you to define a custom container.
+
+'docs.python.org/3/tutorial/classes.html'
+
--- Beginner/CS50/Lecture/Lecture_8/student.py
+++ Beginner/CS50/Lecture/Lecture_8/student.py
@@ -79,3 +79,41 @@ def get_student():
 
 if __name__ == "__main__":
     main()
+
+## chaging list.
+
+def main():
+    student = get_student()
+    if student["name"] == "Padma":
+        student["house"] = "Ravenclaw"
+    print(f"{student['name']} from {student['house']}")
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return {"name": name, "house": house}
+
+
+if __name__ == "__main__":
+    main()
+
+# using classes
+
+class Student:
+    ...
+
+
+
+def main():
+    student = get_student()
+    print(f"{student.name} from {student.house}")
+
+def get_student():
+    student = Student()
+    student.name = input("Name: ")
+    student.house = input("House: ")
+    return student
+
+
+if __name__ == "__main__":
+    main()
