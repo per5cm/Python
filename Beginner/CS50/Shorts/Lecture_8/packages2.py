8a0526f28b96631e1d244b8b45f0611ab6cbd2df
per5cm <per5cm@proton.me>
2024-10-02 14:37:26 +0100

    instance method

--- Beginner/CS50/Shorts/Lecture_8/packages2.py
+++ Beginner/CS50/Shorts/Lecture_8/packages2.py
@@ -1,9 +1,17 @@
+# instance method
+
 class Package:
     def __init__(self, number, sender, recipient, weight):
         self.number = number
         self.sender = sender
         self.recipient = recipient
         self.weight = weight
+    
+    def __str__(self):
+        return f"Package {self.number}: {self.sender} to {self.recipient}, {self.weight}kg"
+    
+    def calculate_cost(self, cost_per_kg):
+        return self.weight * cost_per_kg
         
 def main():
     packages = [
@@ -11,7 +19,7 @@ def main():
         Package(number=2, sender="Bob", recipient="Charlie", weight=5)
     ]
     for package in packages:
-        print(f"Package {package.number}: {package.sender} to {package.recipient}, {package.weight}kg")
+        print(f"{package} costs ${package.calculate_cost(cost_per_kg=2)}")
     
     
 main()
