6700fb0fc1e596218ab7d49c7401ea26ce76ed60
per5cm <per5cm@proton.me>
2024-10-22 11:38:25 +0200

    update, hosue.py

---
+++ Beginner/CS50/Lecture/Lecture_9/houses.py
@@ -0,0 +1,7 @@
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+    {"name": "Padma", "house": "Ravenclaw"},
+]
