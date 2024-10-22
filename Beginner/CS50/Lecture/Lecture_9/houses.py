47f55b3ddd52ce5c9d5db408591dd4c117c0f3e1
per5cm <per5cm@proton.me>
2024-10-22 11:44:45 +0200

    Update houses.py

--- Beginner/CS50/Lecture/Lecture_9/houses.py
+++ Beginner/CS50/Lecture/Lecture_9/houses.py
@@ -1,3 +1,5 @@
+## Initial code
+
 students = [
     {"name": "Hermione", "house": "Gryffindor"},
     {"name": "Harry", "house": "Gryffindor"},
@@ -5,3 +7,28 @@ students = [
     {"name": "Draco", "house": "Slytherin"},
     {"name": "Padma", "house": "Ravenclaw"},
 ]
+
+houses = []
+for student in students:
+    if student["house"] not in houses:
+        houses.append(student["house"])
+
+for house in sorted(houses):
+    print(house)
+
+## Updated Version using set.
+
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+    {"name": "Padma", "house": "Ravenclaw"},
+]
+
+houses = set()
+for student in students:
+    houses.add(student["house"])
+
+for house in sorted(houses):
+    print(house)
