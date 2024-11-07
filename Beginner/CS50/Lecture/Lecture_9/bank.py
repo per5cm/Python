d731fec4e8255cb1cd6c0daa3419c42e41fbb0a6
per5cm <per5cm@proton.me>
2024-11-07 12:45:52 +0100

    update bank.py

--- Beginner/CS50/Lecture/Lecture_9/bank.py
+++ Beginner/CS50/Lecture/Lecture_9/bank.py
@@ -1,8 +1,38 @@
+# initial version
+
 balance = 0
 
 def main():
     print("Balance: ", balance)
+    deposit(100)
+    withdraw(50)
+    print("Balance: ", balance)
+
+def deposit(n):
+    balance += n
+
+def withdraw(n):
+    balance -= n
 
 
 if __name__ == "__main__":
     main()
+
+# fixing errors and next version
+
+def main():
+    balance = 0
+    print("Balance: ", balance)
+    deposit(100)
+    withdraw(50)
+    print("Balance: ", balance)
+
+def deposit(n):
+    balance += n
+
+def withdraw(n):
+    balance -= n
+
+
+if __name__ == "__main__":
+    main() 
